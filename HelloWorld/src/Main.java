0e899e625ed2ab4bd8fbc35fbb75540d8d8756c8
nrjmckay <50786366+nrjmckay@users.noreply.github.com>
2019-05-21 01:18:25 -0600

    Delete Main.java

--- HelloWorld/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
- public static void main(String[] args) {
-  System.out.println("hello here");
-  System.out.println("hello edit");
-  System.out.println("hello from drjava");
- }
-}
